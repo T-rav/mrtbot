a0d1ff820f658fd9cd2ca50b37c2fc7ad69aa1bc
Travis Frisinger <tmfrisinger@gmail.com>
2017-01-08 19:21:31 +0400

    Bot updates for deployment

--- bin/bot.js
+++ bin/bot.js
@@ -8,7 +8,7 @@ var botInstance = new MrtBot({
     token: botToken,
     name: 'mr.t',
     dbPath: 'data/mrtbot.db',
-    joinChannel: 'mrt_test_channel',
+    joinChannel: 'general',
     cronSchedule: '0 30 10 * * 1-5',
     printToLog: true
 });
